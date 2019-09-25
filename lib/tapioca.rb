aa2c68e71e1bc6cff2557d78f9223de61039d75c
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2019-09-25 22:14:55 +0300

    Disable runtime checking safely

--- lib/tapioca.rb
+++ lib/tapioca.rb
@@ -18,10 +18,17 @@ module Tapioca
   class Error < StandardError; end
 end
 
-T::Configuration.default_checked_level = :never
-# Suppresses errors caused by T.cast, T.let, T.must, etc.
-T::Configuration.inline_type_error_handler = ->(*) {}
-# Suppresses errors caused by incorrect parameter ordering
-T::Configuration.sig_validation_error_handler = ->(*) {}
+begin
+  T::Configuration.default_checked_level = :never
+  # Suppresses errors caused by T.cast, T.let, T.must, etc.
+  T::Configuration.inline_type_error_handler = ->(*) {}
+  # Suppresses errors caused by incorrect parameter ordering
+  T::Configuration.sig_validation_error_handler = ->(*) {}
+rescue
+  # Need this rescue so that if another gem has
+  # already set the checked level by the time we
+  # get to it, we don't fail outright.
+  nil
+end
 
 loader.eager_load
