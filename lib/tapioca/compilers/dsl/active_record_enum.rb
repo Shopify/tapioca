7b3037ac956baac0416696bc1494264326fdce11
Kathy <56687406+katayounHillier@users.noreply.github.com>
2020-06-16 14:35:20 -0400

    Extract ActiveRecord::Enum RBI generator into Tapioca (#77)
    * Extract ActiveRecord::Enum RBI generator into Tapioca
    
    * Add more tests, code review changes
    
    * change integer to string type

---
+++ lib/tapioca/compilers/dsl/active_record_enum.rb
@@ -0,0 +1,112 @@
+# typed: true
+# frozen_string_literal: true
+
+require "parlour"
+
+begin
+  require "active_record"
+rescue LoadError
+  # means ActiveRecord is not installed,
+  # so let's not even define the generator.
+  return
+end
+
+module Tapioca
+  module Compilers
+    module Dsl
+      # `Tapioca::Compilers::Dsl::ActiveRecordEnum` decorates RBI files for subclasses of
+      # `ActiveRecord::Base` which declare `enum` fields
+      # (see https://api.rubyonrails.org/classes/ActiveRecord/Enum.html).
+      #
+      # For example, with the following `ActiveRecord::Base` subclass:
+      #
+      # ~~~rb
+      # class Post < ApplicationRecord
+      #   enum title_type: %i(book all web), _suffix: :title
+      # end
+      # ~~~
+      #
+      # this generator will produce the RBI file `post.rbi` with the following content:
+      #
+      # ~~~rbi
+      # # post.rbi
+      # # typed: true
+      # class Post
+      #   sig { void }
+      #   def all_title!; end
+      #
+      #   sig { returns(T::Boolean) }
+      #   def all_title?; end
+      #
+      #   sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+      #   def self.title_types; end
+      #
+      #   sig { void }
+      #   def book_title!; end
+      #
+      #   sig { returns(T::Boolean) }
+      #   def book_title?; end
+      #
+      #   sig { void }
+      #   def web_title!; end
+      #
+      #   sig { returns(T::Boolean) }
+      #   def web_title?; end
+      # end
+      # ~~~
+      class ActiveRecordEnum < Base
+        extend T::Sig
+
+        sig { override.params(root: Parlour::RbiGenerator::Namespace, constant: T.class_of(::ActiveRecord::Base)).void }
+        def decorate(root, constant)
+          return if constant.defined_enums.empty?
+
+          module_name = "#{constant}::EnumMethodsModule"
+          root.create_module(module_name) do |mod|
+            generate_instance_methods(constant, mod)
+          end
+
+          root.path(constant) do |k|
+            k.create_include(module_name)
+
+            constant.defined_enums.each do |name, enum_map|
+              type = type_for_enum(enum_map)
+              create_method(k, name.pluralize, class_method: true, return_type: type)
+            end
+          end
+        end
+
+        sig { override.returns(T::Enumerable[Module]) }
+        def gather_constants
+          ::ActiveRecord::Base.descendants.reject(&:abstract_class?)
+        end
+
+        private
+
+        sig { params(enum_map: T::Hash[T.untyped, T.untyped]).returns(String) }
+        def type_for_enum(enum_map)
+          value_type = enum_map.values.map { |v| v.class.name }.uniq
+          value_type = if value_type.length == 1
+            value_type.first
+          else
+            "T.any(#{value_type.join(', ')})"
+          end
+
+          "T::Hash[T.any(String, Symbol), #{value_type}]"
+        end
+
+        sig { params(constant: T.class_of(::ActiveRecord::Base), klass: Parlour::RbiGenerator::Namespace).void }
+        def generate_instance_methods(constant, klass)
+          methods = constant.send(:_enum_methods_module).instance_methods
+
+          methods.each do |method|
+            method = method.to_s
+            return_type = "T::Boolean" if method.end_with?("?")
+
+            create_method(klass, method, return_type: return_type)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/tapioca/compilers/dsl/active_record_enum_spec.rb
@@ -0,0 +1,353 @@
+# typed: false
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe("Tapioca::Compilers::Dsl::ActiveRecordEnum") do
+  before(:each) do
+    require "tapioca/compilers/dsl/active_record_enum"
+  end
+
+  subject do
+    Tapioca::Compilers::Dsl::ActiveRecordEnum.new
+  end
+
+  describe("#initialize") do
+    def constants_from(content)
+      with_content(content) do
+        subject.processable_constants.map(&:to_s).sort
+      end
+    end
+
+    it("gathers no constants if there are no ActiveRecord classes") do
+      assert_empty(subject.processable_constants)
+    end
+
+    it("gathers only ActiveRecord constants with no abstract classes") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+        end
+
+        class Product < ActiveRecord::Base
+          self.abstract_class = true
+        end
+
+        class User
+        end
+      RUBY
+
+      assert_equal(constants_from(content), ["Conversation"])
+    end
+  end
+
+  describe("#decorate") do
+    def rbi_for(content)
+      with_content(content) do
+        parlour = Parlour::RbiGenerator.new(sort_namespaces: true)
+        subject.decorate(parlour.root, Conversation)
+        parlour.rbi
+      end
+    end
+
+    it("generates RBI file for classes with an enum attribute") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: [ :active, :archived ]
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active!; end
+
+          sig { returns(T::Boolean) }
+          def active?; end
+
+          sig { void }
+          def archived!; end
+
+          sig { returns(T::Boolean) }
+          def archived?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with an enum attribute with string values") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: { active: "0", archived: "1" }
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), String]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active!; end
+
+          sig { returns(T::Boolean) }
+          def active?; end
+
+          sig { void }
+          def archived!; end
+
+          sig { returns(T::Boolean) }
+          def archived?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with an enum attribute with mix value types") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: { active: 0, archived: true, inactive: "Inactive" }
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), T.any(Integer, TrueClass, String)]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active!; end
+
+          sig { returns(T::Boolean) }
+          def active?; end
+
+          sig { void }
+          def archived!; end
+
+          sig { returns(T::Boolean) }
+          def archived?; end
+
+          sig { void }
+          def inactive!; end
+
+          sig { returns(T::Boolean) }
+          def inactive?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with multiple enum attributes") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: [ :active, :archived ]
+          enum comments_status: [:on, :off]
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+          def self.comments_statuses; end
+
+          sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active!; end
+
+          sig { returns(T::Boolean) }
+          def active?; end
+
+          sig { void }
+          def archived!; end
+
+          sig { returns(T::Boolean) }
+          def archived?; end
+
+          sig { void }
+          def off!; end
+
+          sig { returns(T::Boolean) }
+          def off?; end
+
+          sig { void }
+          def on!; end
+
+          sig { returns(T::Boolean) }
+          def on?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with multiple enum attributes with mix value types") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: { active: 0, archived: true, inactive: "Inactive" }
+          enum comments_status: { on: 0, off: false, ongoing: "Ongoing", topic: [1,2,3] }
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), T.any(Integer, FalseClass, String, Array)]) }
+          def self.comments_statuses; end
+
+          sig { returns(T::Hash[T.any(String, Symbol), T.any(Integer, TrueClass, String)]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active!; end
+
+          sig { returns(T::Boolean) }
+          def active?; end
+
+          sig { void }
+          def archived!; end
+
+          sig { returns(T::Boolean) }
+          def archived?; end
+
+          sig { void }
+          def inactive!; end
+
+          sig { returns(T::Boolean) }
+          def inactive?; end
+
+          sig { void }
+          def off!; end
+
+          sig { returns(T::Boolean) }
+          def off?; end
+
+          sig { void }
+          def on!; end
+
+          sig { returns(T::Boolean) }
+          def on?; end
+
+          sig { void }
+          def ongoing!; end
+
+          sig { returns(T::Boolean) }
+          def ongoing?; end
+
+          sig { void }
+          def topic!; end
+
+          sig { returns(T::Boolean) }
+          def topic?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with enum attribute with suffix specified") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: [:active, :archived], _suffix: true
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def active_status!; end
+
+          sig { returns(T::Boolean) }
+          def active_status?; end
+
+          sig { void }
+          def archived_status!; end
+
+          sig { returns(T::Boolean) }
+          def archived_status?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+
+    it("generates RBI file for classes with enum attribute with prefix specified") do
+      content = <<~RUBY
+        class Conversation < ActiveRecord::Base
+          enum status: [:active, :archived], _prefix: :comments
+        end
+
+      RUBY
+
+      expected = <<~RUBY
+        # typed: strong
+        class Conversation
+          include Conversation::EnumMethodsModule
+
+          sig { returns(T::Hash[T.any(String, Symbol), Integer]) }
+          def self.statuses; end
+        end
+
+        module Conversation::EnumMethodsModule
+          sig { void }
+          def comments_active!; end
+
+          sig { returns(T::Boolean) }
+          def comments_active?; end
+
+          sig { void }
+          def comments_archived!; end
+
+          sig { returns(T::Boolean) }
+          def comments_archived?; end
+        end
+      RUBY
+
+      assert_equal(rbi_for(content), expected)
+    end
+  end
+end
