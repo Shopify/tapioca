c18d1aaed6793694b9cab6afe7763158eeb5adfb
Nathan Smith <nathan@nathansmith.io>
2021-03-05 09:36:52 -0800

    Rename TPC_SORBET_EXE to TAPIOCA_SORBET_EXE

--- spec/tapioca/compilers/sorbet_spec.rb
+++ spec/tapioca/compilers/sorbet_spec.rb
@@ -6,22 +6,22 @@ require "spec_helper"
 class Tapioca::Compilers::SorbetSpec < Minitest::Spec
   before do
     @temp_env_value = T.let(nil, T.nilable(String))
-    @temp_env_value = ENV["TPC_SORBET_EXE"]
+    @temp_env_value = ENV["TAPIOCA_SORBET_EXE"]
   end
 
   after do
-    ENV["TPC_SORBET_EXE"] = @temp_env_value
+    ENV["TAPIOCA_SORBET_EXE"] = @temp_env_value
   end
 
-  it("returns the value of TPC_SORBET_EXE if set") do
+  it("returns the value of TAPIOCA_SORBET_EXE if set") do
     custom_path = 'bin/custom-sorbet-static'
-    ENV["TPC_SORBET_EXE"] = custom_path
+    ENV["TAPIOCA_SORBET_EXE"] = custom_path
     sorbet_path = Tapioca::Compilers::Sorbet.sorbet_path
     assert_equal(sorbet_path, custom_path)
   end
 
-  it("returns the default sorbet path if TPC_SORBET_EXE is not set") do
-    ENV["TPC_SORBET_EXE"] = ''
+  it("returns the default sorbet path if TAPIOCA_SORBET_EXE is not set") do
+    ENV["TAPIOCA_SORBET_EXE"] = ''
     gem_path = Gem::Specification.find_by_name("sorbet-static").full_gem_path
     default_path = Tapioca::Compilers::Sorbet::SORBET
     sorbet_path = Tapioca::Compilers::Sorbet.sorbet_path
