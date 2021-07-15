c08e30dc9c7fabe6ca4856d5d0d529ba4f8812b9
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2021-07-15 15:45:01 -0400

    Split cli specs
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

---
+++ spec/tapioca/cli/deprecations_spec.rb
@@ -0,0 +1,25 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative "../cli_spec"
+
+module Tapioca
+  class DeprecationsSpec < CliSpec
+    describe("deprecations") do
+      it "prints the correct deprecation message with -c" do
+        output = execute("dsl", ["-c", "foo"])
+        assert_includes(output, "DEPRECATION: The `-c` and `--cmd` flags will be removed in a future release.")
+      end
+
+      it "prints the correct deprecation message with --cmd" do
+        output = execute("dsl", ["--cmd", "foo"])
+        assert_includes(output, "DEPRECATION: The `-c` and `--cmd` flags will be removed in a future release.")
+      end
+
+      it "doesn't print the correct deprecation message with no flag" do
+        output = execute("dsl")
+        refute_includes(output, "DEPRECATION: The `-c` and `--cmd` flags will be removed in a future release.")
+      end
+    end
+  end
+end
---
+++ spec/tapioca/cli/require_spec.rb
@@ -0,0 +1,84 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative "../cli_spec"
+
+module Tapioca
+  class RequireSpec < CliSpec
+    describe("#require") do
+      before do
+        execute("init")
+      end
+
+      it "does nothing if there is nothing to require" do
+        File.write(repo_path / "sorbet/config", <<~CONFIG)
+          .
+          --ignore=postrequire.rb
+          --ignore=postrequire_faulty.rb
+          --ignore=config/
+        CONFIG
+
+        output = execute("require")
+
+        assert_equal(<<~OUTPUT, output)
+          Compiling sorbet/tapioca/require.rb, this may take a few seconds... Nothing to do
+        OUTPUT
+
+        assert_path_exists(repo_path / "sorbet/tapioca/require.rb")
+        assert_equal(<<~CONTENTS, File.read(repo_path / "sorbet/tapioca/require.rb"))
+          # typed: true
+          # frozen_string_literal: true
+
+          # Add your extra requires here (`tapioca require` can be used to boostrap this list)
+        CONTENTS
+      end
+
+      it "creates a list of all requires from all Ruby files passed to Sorbet" do
+        output = execute("require")
+
+        assert_equal(<<~OUTPUT, output)
+          Compiling sorbet/tapioca/require.rb, this may take a few seconds... Done
+          All requires from this application have been written to sorbet/tapioca/require.rb.
+          Please review changes and commit them, then run `bin/tapioca sync`.
+        OUTPUT
+
+        assert_path_exists(repo_path / "sorbet/tapioca/require.rb")
+        assert_equal(<<~CONTENTS, File.read(repo_path / "sorbet/tapioca/require.rb"))
+          # typed: true
+          # frozen_string_literal: true
+
+          require "active_support/all"
+          require "baz"
+          require "foo/secret"
+          require "foo/will_fail"
+          require "sidekiq"
+          require "smart_properties"
+        CONTENTS
+      end
+
+      it "takes into account sorbet ignored paths" do
+        File.write(repo_path / "sorbet/config", <<~CONFIG)
+          .
+          --ignore=postrequire_faulty.rb
+          --ignore=config/
+        CONFIG
+
+        output = execute("require")
+
+        assert_equal(<<~OUTPUT, output)
+          Compiling sorbet/tapioca/require.rb, this may take a few seconds... Done
+          All requires from this application have been written to sorbet/tapioca/require.rb.
+          Please review changes and commit them, then run `bin/tapioca sync`.
+        OUTPUT
+
+        assert_path_exists(repo_path / "sorbet/tapioca/require.rb")
+        assert_equal(<<~CONTENTS, File.read(repo_path / "sorbet/tapioca/require.rb"))
+          # typed: true
+          # frozen_string_literal: true
+
+          require "foo/secret"
+        CONTENTS
+      end
+    end
+  end
+end
