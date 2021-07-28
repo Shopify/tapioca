729f9833076bba35a4ccefea2c6f31d9fedb3b00
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2021-07-28 20:27:18 +0300

    Does sort do anything?

---
+++ spec/tapioca/rbi/rewriters/sort_nodes_spec.rb
@@ -0,0 +1,49 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Tapioca
+  module RBI
+    class SortNodesSpec < Minitest::HooksSpec
+      describe("can sort nodes") do
+        it("sort nodes in tree") do
+          rbi = RBI::Tree.new
+          rbi << RBI::Const.new("C", "42")
+          rbi << RBI::Module.new("S1")
+          rbi << RBI::Class.new("S2")
+          rbi << RBI::Method.new("m2", is_singleton: true)
+          rbi << RBI::Method.new("m3")
+          rbi << RBI::Method.new("m1")
+          rbi << RBI::Extend.new("E")
+          rbi << RBI::Include.new("I")
+          rbi << RBI::MixesInClassMethods.new("MICM")
+          rbi << RBI::Helper.new("h")
+          rbi << RBI::TStructConst.new("SC", "Type")
+          rbi << RBI::TStructProp.new("SP", "Type")
+          rbi << RBI::TEnum.new("TE")
+          rbi << RBI::TStruct.new("TS")
+
+          rbi.sort_nodes!
+
+          assert_equal(<<~RBI, rbi.string)
+            C = 42
+            module S1; end
+            class S2; end
+            def self.m2; end
+            def m3; end
+            def m1; end
+            extend E
+            include I
+            mixes_in_class_methods MICM
+            h!
+            const :SC, Type
+            prop :SP, Type
+            class TE < ::T::Enum; end
+            class TS < ::T::Struct; end
+          RBI
+        end
+      end
+    end
+  end
+end
