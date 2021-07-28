800fbefde067de2e54df57943aab7461a050bf21
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2021-07-28 20:34:27 +0300

    Sorting works fine without mixins

--- spec/tapioca/rbi/rewriters/sort_nodes_spec.rb
+++ spec/tapioca/rbi/rewriters/sort_nodes_spec.rb
@@ -43,6 +43,37 @@ module Tapioca
             class TS < ::T::Struct; end
           RBI
         end
+
+        it("sort nodes without mixins in tree") do
+          rbi = RBI::Tree.new
+          rbi << RBI::Const.new("C", "42")
+          rbi << RBI::Module.new("S1")
+          rbi << RBI::Class.new("S2")
+          rbi << RBI::Method.new("m2", is_singleton: true)
+          rbi << RBI::Method.new("m3")
+          rbi << RBI::Method.new("m1")
+          rbi << RBI::Helper.new("h")
+          rbi << RBI::TStructConst.new("SC", "Type")
+          rbi << RBI::TStructProp.new("SP", "Type")
+          rbi << RBI::TEnum.new("TE")
+          rbi << RBI::TStruct.new("TS")
+
+          rbi.sort_nodes!
+
+          assert_equal(<<~RBI, rbi.string)
+            h!
+            const :SC, Type
+            prop :SP, Type
+            def m1; end
+            def m3; end
+            C = 42
+            module S1; end
+            class S2; end
+            class TE < ::T::Enum; end
+            class TS < ::T::Struct; end
+            def self.m2; end
+          RBI
+        end
       end
     end
   end
