27eb8066f1b3b6e44d3c0f351065163ad4900404
Cory Hutchison <cory.hutchison@shopify.com>
2021-09-03 14:21:52 -0600

    Migrate `TodoGeneratorSpec` from `TodoSpec`
    Signed-off-by: Cory Hutchison <cory.hutchison@shopify.com>

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -10,13 +10,21 @@ module Tapioca
 
     before(:all) do
       @repo_path = (Pathname.new(__dir__) / "support" / "repo").expand_path
+      Bundler.with_unbundled_env do
+        IO.popen(["bundle", "install", "--quiet"], chdir: @repo_path).read
+        IO.popen(["bundle", "exec", "tapioca", "init"], chdir: @repo_path).read
+      end
     end
 
     around(:each) do |&blk|
+      FileUtils.rm_rf(T.unsafe(self).repo_path / "sorbet") if Dir.exist?(T.unsafe(self).repo_path / "sorbet")
+      FileUtils.rm(T.unsafe(self).repo_path / "Gemfile.lock") if File.exist?(T.unsafe(self).repo_path / "Gemfile.lock")
       Dir.mktmpdir do |outdir|
         @outdir = outdir
         super(&blk)
       end
+      FileUtils.rm_rf(T.unsafe(self).repo_path / "sorbet") if Dir.exist?(T.unsafe(self).repo_path / "sorbet")
+      FileUtils.rm(T.unsafe(self).repo_path / "Gemfile.lock") if File.exist?(T.unsafe(self).repo_path / "Gemfile.lock")
     end
   end
 end
