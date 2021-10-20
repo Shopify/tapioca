3669ac9967d3a5ecc13aecf7982dfcbc7d2bc22d
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2021-10-21 00:30:41 +0300

    Fix test that checks extensions to core types
    Stop expecting that unrelated mixins should be generated on the core
    types we have to reopen them in RBI files.
    
    Co-authored-by: Alexandre Terrasa <583144+Morriar@users.noreply.github.com>

--- spec/tapioca/compilers/symbol_table_compiler_spec.rb
+++ spec/tapioca/compilers/symbol_table_compiler_spec.rb
@@ -243,42 +243,69 @@ class Tapioca::Compilers::SymbolTableCompilerSpec < Minitest::HooksSpec
           def bar
             "bar"
           end
+
+          module Bar; end
         end
       RUBY
 
       add_ruby_file("ext.rb", <<~RUBY)
+
         class String
+          include Foo::Bar
+
           def to_foo(base = "def")
             "abc" + base
           end
         end
 
         class Hash
+          extend Foo::Bar
+
           def to_bar
             {}
           end
         end
+
+        class Array
+          prepend Foo::Bar
+
+          def foo_int; end
+        end
+
+        class Module
+          def bar; end
+        end
       RUBY
 
       output = template(<<~RBI)
+        class Array
+          include ::Foo::Bar
+          include ::Enumerable
+
+          def foo_int; end
+        end
+
         class Foo
           def bar; end
           def to_s; end
         end
 
+        module Foo::Bar; end
+
         class Hash
           include ::Enumerable
-          include ::JSON::Ext::Generator::GeneratorMethods::Hash
+          extend ::Foo::Bar
 
           def to_bar; end
         end
 
+        class Module
+          def bar; end
+        end
+
         class String
           include ::Comparable
-          include ::Colorize::InstanceMethods
-          include ::JSON::Ext::Generator::GeneratorMethods::String
-          extend ::Colorize::ClassMethods
-          extend ::JSON::Ext::Generator::GeneratorMethods::String::Extend
+          include ::Foo::Bar
 
           def to_foo(base = T.unsafe(nil)); end
         end
