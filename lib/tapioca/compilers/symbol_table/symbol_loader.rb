6820fbfdd28bbcd02f2c10de74f8c0525e23112f
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-01-31 12:34:50 -0500

    Extract SymbolTableParser
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/tapioca/compilers/symbol_table/symbol_loader.rb
+++ lib/tapioca/compilers/symbol_table/symbol_loader.rb
@@ -35,15 +35,13 @@ module Tapioca
 
             return Set.new if output.nil? || output.empty?
 
-            json = JSON.parse(output)
-            SymbolTableParser.parse(json)
+            SymbolTableParser.parse_json(output)
           end
 
           def ignored_symbols
             unless @ignored_symbols
               output = symbol_table_json_from("-e ''", table_type: "symbol-table-full-json")
-              json = JSON.parse(output)
-              @ignored_symbols = SymbolTableParser.parse(json)
+              @ignored_symbols = SymbolTableParser.parse_json(output)
             end
 
             @ignored_symbols
@@ -51,38 +49,6 @@ module Tapioca
 
           def symbol_table_json_from(input, table_type: "symbol-table-json")
             sorbet("--no-config", "--print=#{table_type}", input)
-          end
-        end
-
-        class SymbolTableParser
-          def self.parse(object, parents = [])
-            symbols = Set.new
-
-            children = object.fetch("children", [])
-
-            children.each do |child|
-              kind = child.fetch("kind")
-              name = child.fetch("name")
-              name = name.fetch("name") if name.is_a?(Hash)
-
-              next if kind.nil? || name.nil?
-
-              # TODO: CLASS is removed since v0.4.4730 of Sorbet
-              # but keeping here for backward compatibility. Remove
-              # once the minimum version is moved past that.
-              next unless ["CLASS", "CLASS_OR_MODULE", "STATIC_FIELD"].include?(kind)
-              next if name =~ /[<>()$]/
-              next if name =~ /^[0-9]+$/
-              next if name == "T::Helpers"
-
-              parents << name
-
-              symbols.add(parents.join("::"))
-              symbols.merge(parse(child, parents))
-
-              parents.pop
-            end
-            symbols
           end
         end
       end
