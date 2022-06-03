9ba3c8c6bfc7f104f88239a812497dc9c283bc2b
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-06-03 12:27:04 -0400

    Add test for `annotations` using the default source over https
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- spec/tapioca/cli/annotations_spec.rb
+++ spec/tapioca/cli/annotations_spec.rb
@@ -100,6 +100,13 @@ module Tapioca
         repo.destroy
       end
 
+      it "gets index from the central repo using the default source" do
+        result = @project.tapioca("annotations")
+
+        assert_includes(result.out, "Retrieving index from central repository... Done")
+        assert_success_status(result)
+      end
+
       it "handles parse errors within annotations" do
         repo = create_repo({
           spoom: <<~RBI,
