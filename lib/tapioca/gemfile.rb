105a5e5cb9dffda0647db2dc9913c7dd4c3fde0d
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-06-06 19:11:35 -0400

    Require `bundler` at the top level
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/tapioca/gemfile.rb
+++ lib/tapioca/gemfile.rb
@@ -1,7 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-require "bundler"
 require "logger"
 require "yard-sorbet"
 
--- lib/tapioca/internal.rb
+++ lib/tapioca/internal.rb
@@ -1,6 +1,7 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "bundler"
 require "erb"
 require "etc"
 require "fileutils"
