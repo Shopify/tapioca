0b2de5212ec46a066cefc2554edc7baea2c08193
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-06-06 20:32:10 -0400

    Merge `ParamHelper` into `RBIHelper`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/tapioca/helpers/rbi_helper.rb
+++ lib/tapioca/helpers/rbi_helper.rb
@@ -6,6 +6,46 @@ module Tapioca
     extend T::Sig
     include SorbetHelper
 
+    sig { params(name: String, type: String).returns(RBI::TypedParam) }
+    def create_param(name, type:)
+      create_typed_param(RBI::Param.new(name), type)
+    end
+
+    sig { params(name: String, type: String, default: String).returns(RBI::TypedParam) }
+    def create_opt_param(name, type:, default:)
+      create_typed_param(RBI::OptParam.new(name, default), type)
+    end
+
+    sig { params(name: String, type: String).returns(RBI::TypedParam) }
+    def create_rest_param(name, type:)
+      create_typed_param(RBI::RestParam.new(name), type)
+    end
+
+    sig { params(name: String, type: String).returns(RBI::TypedParam) }
+    def create_kw_param(name, type:)
+      create_typed_param(RBI::KwParam.new(name), type)
+    end
+
+    sig { params(name: String, type: String, default: String).returns(RBI::TypedParam) }
+    def create_kw_opt_param(name, type:, default:)
+      create_typed_param(RBI::KwOptParam.new(name, default), type)
+    end
+
+    sig { params(name: String, type: String).returns(RBI::TypedParam) }
+    def create_kw_rest_param(name, type:)
+      create_typed_param(RBI::KwRestParam.new(name), type)
+    end
+
+    sig { params(name: String, type: String).returns(RBI::TypedParam) }
+    def create_block_param(name, type:)
+      create_typed_param(RBI::BlockParam.new(name), type)
+    end
+
+    sig { params(param: RBI::Param, type: String).returns(RBI::TypedParam) }
+    def create_typed_param(param, type)
+      RBI::TypedParam.new(param: param, type: sanitize_signature_types(type))
+    end
+
     sig { params(sig_string: String).returns(String) }
     def sanitize_signature_types(sig_string)
       sig_string
