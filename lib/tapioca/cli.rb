4fb0d25a3e8c524953363e18cb719bda973b93fb
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2022-06-14 16:30:26 -0400

    Move check-shims code as a command
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- lib/tapioca/cli.rb
+++ lib/tapioca/cli.rb
@@ -5,9 +5,7 @@ module Tapioca
   class Cli < Thor
     include CliHelper
     include ConfigHelper
-    include SorbetHelper
     include EnvHelper
-    include RBIFilesHelper
 
     FILE_HEADER_OPTION_DESC = "Add a \"This file is generated\" header on top of each generated RBI file"
 
@@ -261,60 +259,13 @@ module Tapioca
     option :shim_rbi_dir, type: :string, desc: "Path to shim RBIs", default: DEFAULT_SHIM_DIR
     option :payload, type: :boolean, desc: "Check shims against Sorbet's payload", default: true
     def check_shims
-      index = RBI::Index.new
-
-      shim_rbi_dir = options[:shim_rbi_dir]
-      if !Dir.exist?(shim_rbi_dir) || Dir.empty?(shim_rbi_dir)
-        say("No shim RBIs to check", :green)
-        exit(0)
-      end
-
-      payload_path = T.let(nil, T.nilable(String))
-
-      if options[:payload]
-        if sorbet_supports?(:print_payload_sources)
-          Dir.mktmpdir do |dir|
-            payload_path = dir
-            result = sorbet("--no-config --print=payload-sources:#{payload_path}")
-
-            unless result.status
-              say_error("Sorbet failed to dump payload")
-              say_error(result.err)
-              exit(1)
-            end
-
-            index_payload(index, payload_path)
-          end
-        else
-          say_error("The version of Sorbet used in your Gemfile.lock does not support `--print=payload-sources`")
-          say_error("Current: v#{SORBET_GEM_SPEC.version}")
-          say_error("Required: #{FEATURE_REQUIREMENTS[:print_payload_sources]}")
-          exit(1)
-        end
-      end
-
-      index_rbis(index, "shim", shim_rbi_dir)
-      index_rbis(index, "gem", options[:gem_rbi_dir])
-      index_rbis(index, "dsl", options[:dsl_rbi_dir])
-
-      duplicates = duplicated_nodes_from_index(index, shim_rbi_dir)
-      unless duplicates.empty?
-        duplicates.each do |key, nodes|
-          say_error("\nDuplicated RBI for #{key}:", :red)
-          nodes.each do |node|
-            node_loc = node.loc
-            next unless node_loc
-
-            loc_string = location_to_payload_url(node_loc, path_prefix: payload_path)
-            say_error(" * #{loc_string}", :red)
-          end
-        end
-        say_error("\nPlease remove the duplicated definitions from the #{shim_rbi_dir} directory.", :red)
-        exit(1)
-      end
-
-      say("\nNo duplicates found in shim RBIs", :green)
-      exit(0)
+      command = Commands::CheckShims.new(
+        gem_rbi_dir: options[:gem_rbi_dir],
+        dsl_rbi_dir: options[:dsl_rbi_dir],
+        shim_rbi_dir: options[:shim_rbi_dir],
+        payload: options[:payload]
+      )
+      command.execute
     end
 
     desc "annotations", "Pull gem RBI annotations from remote sources"
