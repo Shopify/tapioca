bb5fd2222fefb1fa39ceed6baffba043ef27b69c
Ufuk Kayserilioglu <ufuk@paralaus.com>
2022-07-08 00:32:26 +0300

    Add more test cases for method name validation

--- spec/tapioca/helpers/rbi_helper_spec.rb
+++ spec/tapioca/helpers/rbi_helper_spec.rb
@@ -13,19 +13,19 @@ class Tapioca::RBIHelperSpec < Minitest::Spec
         "!", "~", "+@", "**", "-@", "*", "/", "%", "+", "-", "<<", ">>", "&", "|", "^",
         "<", "<=", ">", ">=", "==", "===", "!=", "=~", "!~", "<=>", "[]", "[]=", "`",
       ].each do |name|
-        assert(valid_method_name?(name))
+        assert(valid_method_name?(name), "Expected `#{name}` to be a valid method name")
       end
     end
 
     it "rejects invalid method names" do
-      ["", "42", "42foo", "!foo", "-@foo", "foo-", "foo-bar", "foo.bar", "=>"].each do |name|
-        refute(valid_method_name?(name))
+      ["", "42", "42foo", "42foo=", "!foo", "-@foo", "foo-", "foo-bar", "foo.bar", "=>"].each do |name|
+        refute(valid_method_name?(name), "Expected `#{name}` to be an invalid method name")
       end
     end
 
     it "accepts valid parameter names" do
       ["f", "foo", "_foo", "foo_", "fOO", "f00", "éxéctôr", "❨╯°□°❩╯︵┻━┻"].each do |name|
-        assert(valid_parameter_name?(name))
+        assert(valid_parameter_name?(name), "Expected `#{name}` to be a valid parameter name")
       end
     end
 
