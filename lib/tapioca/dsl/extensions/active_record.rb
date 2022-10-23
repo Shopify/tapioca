eb7fdc580be0c729deed838f5b75c9000b4dda51
Frederick Cheung <frederick.cheung@gmail.com>
2022-10-23 20:00:56 +0100

    Add activerecord extension to track delegated types
    ActiveRecord doesn't store any metadata about delegated types, so we need to track
    this ourselves

---
+++ lib/tapioca/dsl/extensions/active_record.rb
@@ -0,0 +1,29 @@
+# typed: true
+# frozen_string_literal: true
+
+begin
+  require "active_record"
+rescue LoadError
+  return
+end
+
+module Tapioca
+  module Dsl
+    module Compilers
+      module Extensions
+        module ActiveRecord
+          attr_reader :__tapioca_delegated_types
+
+          def delegated_type(role, types:, **options)
+            @__tapioca_delegated_types ||= {}
+            @__tapioca_delegated_types[role] = { types: types }
+
+            super
+          end
+
+          ::ActiveRecord::Base.singleton_class.prepend(self)
+        end
+      end
+    end
+  end
+end
