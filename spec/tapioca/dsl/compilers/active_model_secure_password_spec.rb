9772dcc246ec034595ee9f89f5ea520f999f0c0e
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2023-07-20 00:08:21 +0300

    Add password salt getter added in Rails head
    Added in Rails here: https://github.com/rails/rails/pull/47490

--- spec/tapioca/dsl/compilers/active_model_secure_password_spec.rb
+++ spec/tapioca/dsl/compilers/active_model_secure_password_spec.rb
@@ -85,6 +85,9 @@ module Tapioca
 
                   sig { params(_arg0: T.untyped).returns(T.untyped) }
                   def password_confirmation=(_arg0); end
+
+                  sig { returns(T.untyped) }
+                  def password_salt; end
                 <% else %>
                   sig { params(unencrypted_password: T.untyped).returns(T.untyped) }
                   def password_confirmation=(unencrypted_password); end
@@ -129,6 +132,9 @@ module Tapioca
 
                   sig { params(_arg0: T.untyped).returns(T.untyped) }
                   def token_confirmation=(_arg0); end
+
+                  sig { returns(T.untyped) }
+                  def token_salt; end
                 <% else %>
                   sig { params(unencrypted_password: T.untyped).returns(T.untyped) }
                   def token_confirmation=(unencrypted_password); end
@@ -180,6 +186,9 @@ module Tapioca
 
                   sig { params(_arg0: T.untyped).returns(T.untyped) }
                   def password_confirmation=(_arg0); end
+
+                  sig { returns(T.untyped) }
+                  def password_salt; end
                 <% else %>
                   sig { params(unencrypted_password: T.untyped).returns(T.untyped) }
                   def password_confirmation=(unencrypted_password); end
@@ -203,6 +212,9 @@ module Tapioca
 
                   sig { params(_arg0: T.untyped).returns(T.untyped) }
                   def token_confirmation=(_arg0); end
+
+                  sig { returns(T.untyped) }
+                  def token_salt; end
                 <% else %>
                   sig { params(unencrypted_password: T.untyped).returns(T.untyped) }
                   def token_confirmation=(unencrypted_password); end
