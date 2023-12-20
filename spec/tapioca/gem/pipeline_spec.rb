1e3c9ac5ddec6a49b10486095be0baf11e5832b3
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2023-12-20 21:39:42 +0200

    Remove all code related to `non_generic_weak_map` Sorbet flag
    Since https://github.com/sorbet/sorbet/pull/6610 was merged and released as 0.5.10588, we no longer need to check for the existence of non generic `WeakMap` definition in Sorbet. Since we are moving to
    >= 0.5.10820, we can remove all code related to `ObjectSpace::WeakMap[T.untyped]`.

--- spec/tapioca/gem/pipeline_spec.rb
+++ spec/tapioca/gem/pipeline_spec.rb
@@ -2043,11 +2043,7 @@ class Tapioca::Gem::PipelineSpec < Minitest::HooksSpec
       RUBY
 
       output = template(<<~RBI)
-        <% if sorbet_supports?(:non_generic_weak_map) %>
         Foo = T.let(T.unsafe(nil), ObjectSpace::WeakMap)
-        <% else %>
-        Foo = T.let(T.unsafe(nil), ObjectSpace::WeakMap[T.untyped])
-        <% end %>
       RBI
 
       assert_equal(output, compile)
