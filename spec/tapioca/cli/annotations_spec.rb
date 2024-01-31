c2fb4785123ad17efe52d4e101921d60f9ecebed
Emily Samp <emily.samp@shopify.com>
2024-01-31 10:47:03 -0600

    Add test for RBI version filtering

--- spec/tapioca/cli/annotations_spec.rb
+++ spec/tapioca/cli/annotations_spec.rb
@@ -12,6 +12,7 @@ module Tapioca
 
       after do
         @project.remove!("sorbet/rbi/annotations")
+        @project.write!("Gemfile", @project.tapioca_gemfile)
       end
 
       it "does nothing if the repo is empty" do
@@ -352,6 +353,45 @@ module Tapioca
           # Please run `bin/tapioca annotations` to update it.
 
           class AnnotationForSpoom; end
+        RBI
+
+        assert_success_status(result)
+
+        repo.destroy!
+      end
+
+      it "filters RBI file based on gem version" do
+        foo = mock_gem("foo", "0.3.4") do
+          write!("lib/foo.rb", <<~BAR)
+            class Foo; end
+          BAR
+        end
+
+        @project.require_mock_gem(foo)
+        @project.bundle_install!
+
+        repo = create_repo({
+          foo: <<~RBI,
+            # typed: false
+
+            # @version > 0.3.5
+            class AnnotationForFoo; end
+            class Foo; end
+          RBI
+        })
+
+        result = @project.tapioca("annotations --sources #{repo.absolute_path}")
+
+        assert_stdout_includes(result, "create  sorbet/rbi/annotations/foo.rbi")
+
+        assert_project_annotation_equal("sorbet/rbi/annotations/foo.rbi", <<~RBI)
+          # typed: false
+
+          # DO NOT EDIT MANUALLY
+          # This file was pulled from a central RBI files repository.
+          # Please run `bin/tapioca annotations` to update it.
+
+          class Foo; end
         RBI
 
         assert_success_status(result)
