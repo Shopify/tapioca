5c2ce7958f06e616f14e63e8d70bb8f1eb92fd89
Gustavo Maloste <gustavo.maloste@shopify.com>
2024-07-31 13:59:07 -0400

    ActiveRecordRelations support for  generic definitions in AR models

--- lib/tapioca/helpers/rbi_helper.rb
+++ lib/tapioca/helpers/rbi_helper.rb
@@ -6,6 +6,7 @@ module Tapioca
     extend T::Sig
     include SorbetHelper
     extend SorbetHelper
+    include Runtime::Reflection
     extend self
 
     class << self
@@ -137,6 +138,62 @@ module Tapioca
       op == :definemethod && arg == sentinel_method_name && data.dig(11, :keyword, 0) == name.to_sym
     rescue SyntaxError
       false
+    end
+
+    sig { params(constant: T.all(Module, T::Generic)).returns(String) }
+    def generic_name_of(constant)
+      type_name = T.must(qualified_name_of(constant))
+      return type_name if type_name =~ /\[.*\]$/
+
+      type_variables = Runtime::GenericTypeRegistry.lookup_type_variables(constant)
+      return type_name unless type_variables
+
+      type_variables = type_variables.reject(&:fixed?)
+      return type_name if type_variables.empty?
+
+      type_variable_names = type_variables.map { "T.untyped" }.join(", ")
+
+      "#{type_name}[#{type_variable_names}]"
+    end
+
+    sig { params(constant: Module).returns(T.nilable(String)) }
+    def qualified_name_of(constant)
+      name = name_of(constant)
+      return if name.nil?
+
+      if name.start_with?("::")
+        name
+      else
+        "::#{name}"
+      end
+    end
+
+    sig { params(constant: Module).returns(T.nilable(String)) }
+    def name_of(constant)
+      name = name_of_proxy_target(constant, super(class_of(constant)))
+      return name if name
+
+      name = super(constant)
+      return if name.nil?
+      return unless are_equal?(constant, constantize(name, inherit: true))
+
+      name = "Struct" if name =~ /^(::)?Struct::[^:]+$/
+      name
+    end
+
+    sig { params(constant: Module, class_name: T.nilable(String)).returns(T.nilable(String)) }
+    def name_of_proxy_target(constant, class_name)
+      return unless class_name == "ActiveSupport::Deprecation::DeprecatedConstantProxy"
+
+      # We are dealing with a ActiveSupport::Deprecation::DeprecatedConstantProxy
+      # so try to get the name of the target class
+      begin
+        target = constant.__send__(:target)
+      rescue NoMethodError
+        return
+      end
+
+      name_of(target)
     end
   end
 end
