885ff1ab8ddedaf3bdec76eabf47fd25c0a00695
Emily Samp <emily.samp@shopify.com>
2024-09-25 15:26:54 -0500

    Reproduce flipper bug
    When flipper/flipper-active_record version 1.3.1 is included
    in the Gemfile, Flipper-related code gets added to the
    activerecord gemfile.

--- spec/tapioca/cli/gem_spec.rb
+++ spec/tapioca/cli/gem_spec.rb
@@ -648,6 +648,21 @@ module Tapioca
           assert_success_status(result)
         end
 
+        it "must not include flipper definitions in ActiveRecord RBIs" do
+          @project.require_real_gem("rails", "7.1.0")
+          # @project.require_real_gem("flipper", "1.3.1")
+          @project.require_real_gem("flipper-active_record", "1.3.1")
+
+          @project.bundle_install!
+          result = @project.tapioca("gem")
+
+          assert_empty_stderr(result)
+          assert_success_status(result)
+
+          activerecord_rbi_file = T.must(Dir.glob("#{@project.absolute_path}/sorbet/rbi/gems/activerecord@7.1.0.rbi").first)
+          refute_includes(File.read(activerecord_rbi_file), "class Flipper")
+        end
+
         it "must generate multiple gem RBIs" do
           foo = mock_gem("foo", "0.0.1") do
             write!("lib/foo.rb", FOO_RB)
