a58bec69434c4daa642474957a32fc8a5e5d457e
Kaan Ozkan <kaanozkan97@gmail.com>
2024-10-07 14:55:35 -0400

    Add Server Addon boilerplate

---
+++ lib/ruby_lsp/tapioca/server_addon.rb
@@ -0,0 +1,27 @@
+# typed: false
+# frozen_string_literal: true
+
+require "tapioca/internal"
+
+module RubyLsp
+  module Tapioca
+    class ServerAddon < ::RubyLsp::Rails::ServerAddon
+      def name
+        "Tapioca"
+      end
+
+      def execute(request, params)
+        case request
+        when "dsl"
+          dsl(params)
+        end
+      end
+
+      private
+
+      def dsl(params)
+        # TODO
+      end
+    end
+  end
+end
