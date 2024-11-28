ffc509c0b921b18feed9ad34a0aa990229d9dd7e
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-11-28 12:59:11 -0500

    Add test for `lsp_addon` flag in CLI

---
+++ spec/tapioca/cli_spec.rb
@@ -0,0 +1,30 @@
+# typed: true
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Tapioca
+  class CliSpec < SpecWithProject
+    describe "Tapioca::Cli" do
+      before do
+        @command_stub = mock(run: nil)
+      end
+
+      it "passes through the `lsp_addon` flag to the DslGenerate command" do
+        Commands::DslGenerate.expects(:new).with do |options|
+          options[:lsp_addon] == true
+        end.returns(@command_stub)
+
+        capture_io { Cli.start(["dsl", "--lsp_addon"]) }
+      end
+
+      it "does not pass through the `lsp_addon` flag to the DslGenerate command if not present" do
+        Commands::DslGenerate.expects(:new).with do |options|
+          options[:lsp_addon].nil?
+        end.returns(@command_stub)
+
+        capture_io { Cli.start(["dsl", "--another-flag"]) }
+      end
+    end
+  end
+end
