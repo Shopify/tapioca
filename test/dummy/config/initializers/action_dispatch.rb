df25942821377a59cf13c4314930f9ad179c5d5d
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-03 16:01:16 -0500

    Remove unnecessary files

--- test/dummy/config/initializers/action_dispatch.rb
+++
@@ -1,5 +0,0 @@
-# typed: true
-# frozen_string_literal: true
-
-# Route source locations are normally only available in development, so we need to enable this in test mode.
-ActionDispatch::Routing::Mapper.route_source_locations = true
