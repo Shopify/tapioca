4a1fbfd7436357fd828a1e4370d29dbaf391107b
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-09 11:29:43 -0500

    Remove boileerplace [skip ci]

--- test/dummy/config.ru
+++ test/dummy/config.ru
@@ -1,8 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-# This file is used by Rack-based servers to start the application.
-
 require_relative "config/environment"
 
 run Rails.application
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,8 +7,6 @@ $LOAD_PATH.unshift(File.expand_path("../../../lib", __dir__))
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Dummy
