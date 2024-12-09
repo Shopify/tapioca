18120ad4a634bff8d3cebf3fee2bd4c969591623
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-09 11:24:27 -0500

    Remove boot.rb [skip ci]

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "boot"
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
+
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+$LOAD_PATH.unshift(File.expand_path("../../../lib", __dir__))
 
 require "rails/all"
 
