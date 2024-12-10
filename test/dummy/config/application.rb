c430822c6a957e4223d29d1a8e7869551d9af7c8
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-10 11:50:23 -0500

    Remove DB setup [skip ci]

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -5,7 +5,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
 $LOAD_PATH.unshift(File.expand_path("../../../lib", __dir__))
 
-require "rails/all"
+require "rails" # minimal, instead of "rails/all"
 
 Bundler.require(*Rails.groups)
 
