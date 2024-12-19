9cfb25c88288ddbcfbb3fb2f464d4bff3610fc43
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-12-19 13:58:42 -0500

    Add DSL integration tests for add-on

---
+++ spec/dummy/config/application.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
+
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+$LOAD_PATH.unshift(File.expand_path("../../../lib", __dir__))
+
+require "rails" # minimal, instead of "rails/all"
+require "active_job/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Dummy
+  class Application < Rails::Application
+  end
+end
