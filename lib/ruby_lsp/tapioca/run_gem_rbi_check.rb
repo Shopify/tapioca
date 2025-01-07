607be12809ab7479ca510b306731b486de1a333f
Alex Rocha <alex.rocha@shopify.com>
2025-01-06 22:18:25 -0800

    Refactor RunGemRbiCheck for readability

--- lib/ruby_lsp/tapioca/run_gem_rbi_check.rb
+++ lib/ruby_lsp/tapioca/run_gem_rbi_check.rb
@@ -5,14 +5,14 @@ require "ruby_lsp/tapioca/lockfile_diff_parser"
 
 module RubyLsp
   module Tapioca
+    class GemRbiCheckResult < T::Struct
+      prop :stdout, String
+      prop :stderr, String
+      prop :status, T.nilable(Process::Status)
+    end
+
     class RunGemRbiCheck
       extend T::Sig
-
-      class GemRbiCheckResult < T::Struct
-        prop :stdout, String
-        prop :stderr, String
-        prop :status, T.nilable(Process::Status)
-      end
 
       sig { void }
       def initialize
@@ -30,7 +30,7 @@ module RubyLsp
           if git_repo?
             lockfile_changed? ? generate_gem_rbis : cleanup_orphaned_rbis
           else
-            @result.stdout = "Not a git repository"
+            log_message("Not a git repository")
           end
         end
 
@@ -44,7 +44,6 @@ module RubyLsp
         require "open3"
 
         _, status = Open3.capture2e("git rev-parse --is-inside-work-tree")
-
         T.must(status.success?)
       end
 
@@ -62,24 +61,37 @@ module RubyLsp
       sig { void }
       def generate_gem_rbis
         parser = Tapioca::LockfileDiffParser.new(@lockfile_diff)
-
         removed_gems = parser.removed_gems
         added_or_modified_gems = parser.added_or_modified_gems
 
         if added_or_modified_gems.any?
-          # Resetting BUNDLE_GEMFILE to root folder to use the project's Gemfile instead of Ruby LSP's composed Gemfile
-          @result.stdout, @result.stderr, @result.status = T.unsafe(Open3).capture3(
-            { "BUNDLE_GEMFILE" => "Gemfile" },
-            "bin/tapioca",
-            "gem",
-            "--lsp_addon",
-            *added_or_modified_gems,
-          )
+          log_message("Identified lockfile changes, attempting to generate gem RBIs...")
+          execute_tapioca_gem_command(added_or_modified_gems)
         elsif removed_gems.any?
-          FileUtils.rm_f(Dir.glob("sorbet/rbi/gems/{#{removed_gems.join(",")}}@*.rbi"))
-
-          @result.stdout = "Removed RBIs for: #{removed_gems.join(", ")}"
+          remove_rbis(removed_gems)
         end
+      end
+
+      sig { params(gems: T::Array[String]).void }
+      def execute_tapioca_gem_command(gems)
+        # Resetting BUNDLE_GEMFILE to root folder to use the project's Gemfile instead of Ruby LSP's composed Gemfile
+        stdout, stderr, status = T.unsafe(Open3).capture3(
+          { "BUNDLE_GEMFILE" => "Gemfile" },
+          "bin/tapioca",
+          "gem",
+          "--lsp_addon",
+          *gems,
+        )
+
+        log_message(stdout) unless stdout.empty?
+        log_message(stderr) unless stderr.empty?
+        @result.status = status
+      end
+
+      sig { params(gems: T::Array[String]).void }
+      def remove_rbis(gems)
+        FileUtils.rm_f(Dir.glob("sorbet/rbi/gems/{#{gems.join(",")}}@*.rbi"))
+        log_message("Removed RBIs for: #{gems.join(", ")}")
       end
 
       sig { void }
@@ -87,22 +99,25 @@ module RubyLsp
         untracked_files = %x(git ls-files --others --exclude-standard sorbet/rbi/gems/).lines.map(&:strip)
         deleted_files = %x(git ls-files --deleted sorbet/rbi/gems/).lines.map(&:strip)
 
-        deleted_rbis = []
-        restored_rbis = []
+        delete_files(untracked_files, "Deleted untracked RBIs")
+        restore_files(deleted_files, "Restored deleted RBIs")
+      end
 
-        untracked_files.each do |file|
-          File.delete(file)
-          deleted_rbis << file
-        end
+      sig { params(files: T::Array[String], message: String).void }
+      def delete_files(files, message)
+        files.each { |file| File.delete(file) }
+        log_message("#{message}: #{files.join(", ")}") unless files.empty?
+      end
 
-        deleted_files.each do |file|
-          %x(git checkout -- #{file})
-          restored_rbis << file
-        end
+      sig { params(files: T::Array[String], message: String).void }
+      def restore_files(files, message)
+        files.each { |file| %x(git checkout -- #{file}) }
+        log_message("#{message}: #{files.join(", ")}") unless files.empty?
+      end
 
-        @result.stdout = "Deleted untracked RBIs: #{deleted_rbis.join(", ")}" unless deleted_rbis.empty?
-        @result.stdout += "\n" unless deleted_rbis.empty? || restored_rbis.empty?
-        @result.stdout += "Restored deleted RBIs: #{restored_rbis.join(", ")}" unless restored_rbis.empty?
+      sig { params(message: String).void }
+      def log_message(message)
+        @result.stdout += "#{message}\n"
       end
     end
   end
