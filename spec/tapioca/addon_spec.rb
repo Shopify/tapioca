5ddb4422a3400553c91d3c3bdde0829f66bca0ad
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-01-31 15:41:29 -0500

    wip

--- spec/tapioca/addon_spec.rb
+++ spec/tapioca/addon_spec.rb
@@ -10,7 +10,7 @@ require "minitest/hooks"
 
 module RubyLsp
   module Tapioca
-    class AddonSpec < Minitest::HooksSpec
+    class AddonSpec < ::Tapioca::SpecWithProject
       # The approach here is based on tests within the Ruby LSP Rails gem
 
       it "generates DSL RBIs for a given constant" do
@@ -30,7 +30,10 @@ module RubyLsp
       end
 
       it "triggers route DSL generation if routes.rb is modified" do
-        create_client
+        create_rails_project
+        @project.bundle_install!
+
+        create_client(@project.absolute_path)
 
         global_state = RubyLsp::GlobalState.new
         global_state.apply_options({
@@ -41,30 +44,31 @@ module RubyLsp
           },
         })
 
-        addon = Addon.new
-        addon.instance_variable_set(:@rails_runner_client, @client)
-        addon.instance_variable_set(:@global_state, global_state)
-        addon.instance_variable_set(:@index, global_state.index)
-        addon.instance_variable_set(:@outgoing_queue, @outgoing_queue)
-
-        File.write("spec/dummy/config/routes.rb", <<~RUBY)
+        # File.write("spec/dummy/config/routes.rb", <<~RUBY)
+        @project.write!("config/routes.rb", <<~RUBY)
           Rails.application.routes.draw do
           end
         RUBY
 
-        addon.workspace_did_change_watched_files([{
-          uri: "file://#{Dir.pwd}/spec/dummy/config/routes.rb",
-          type: Constant::FileChangeType::CREATED,
-        }])
+        @client.delegate_notification(
+          server_addon_name: "Tapioca",
+          request_name: "route_dsl",
+        )
 
-        wait_until_exists("spec/dummy/sorbet/rbi/dsl/generated_path_helpers_module.rbi")
+        wait_until_exists("#{@project.absolute_path}/sorbet/rbi/dsl/generated_path_helpers_module.rbi")
         shutdown_client
 
-        assert_match("rails_info_routes_path", File.read("spec/dummy/sorbet/rbi/dsl/generated_path_helpers_module.rbi"))
-        assert_match("rails_info_routes_url", File.read("spec/dummy/sorbet/rbi/dsl/generated_url_helpers_module.rbi"))
-      ensure
-        FileUtils.rm_rf("spec/dummy/sorbet/rbi")
-        FileUtils.rm("spec/dummy/config/routes.rb") if File.exist?("spec/dummy/config/routes.rb")
+        assert_match(
+          "rails_info_routes_path",
+          File.read("#{@project.absolute_path}/sorbet/rbi/dsl/generated_path_helpers_module.rbi"),
+        )
+        assert_match(
+          "rails_info_routes_url",
+          File.read("#{@project.absolute_path}/sorbet/rbi/dsl/generated_url_helpers_module.rbi"),
+        )
+        # ensure
+        # FileUtils.rm_rf("spec/dummy/sorbet/rbi")
+        # FileUtils.rm("spec/dummy/config/routes.rb") if File.exist?("spec/dummy/config/routes.rb")
       end
 
       it "triggers ActiveRecordFixtures DSL compiler if a fixture is modified" do
@@ -151,10 +155,10 @@ module RubyLsp
       private
 
       # Starts a new client
-      def create_client
+      def create_client(path = "spec/dummy")
         @outgoing_queue = Thread::Queue.new
         global_state = GlobalState.new
-        @client = FileUtils.chdir("spec/dummy") do
+        @client = FileUtils.chdir(path) do
           RubyLsp::Rails::RunnerClient.new(@outgoing_queue, global_state)
         end
 
@@ -182,6 +186,25 @@ module RubyLsp
         end
       rescue Timeout::Error
         flunk("#{path} was not created in time")
+      end
+
+      def create_rails_project
+        @project.write!("config/environment.rb", <<~RB)
+          require_relative "application.rb"
+        RB
+
+        @project.write!("config/application.rb", <<~RB)
+          require "rails"
+          module Test
+            class Application < Rails::Application
+            end
+          end
+        RB
+
+        @project.require_real_gem("rails")
+
+        content = File.read("spec/dummy/bin/rails")
+        @project.write!("bin/rails", content)
       end
     end
   end
