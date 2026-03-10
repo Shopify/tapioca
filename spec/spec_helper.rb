61965cdcfa8bf87f3d707bce050c02e3e7a8a3f8
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2026-03-10 23:42:59 +0200

    exp3: use default minitest reporter instead of SpecReporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,21 +14,7 @@ require "dsl_spec_helper"
 require "spec_with_project"
 require "rails_spec_helper"
 
-require "minitest/reporters"
-require "spec_reporter"
-
-# Minitest::Reporters currently lacks support for Minitest 6 out of the box
-# but we can register the plugin to use it.
-# Ref: https://github.com/minitest-reporters/minitest-reporters/pull/366#issuecomment-3731951673
-require "minitest/minitest_reporter_plugin"
-Minitest.register_plugin(:minitest_reporter)
-
-backtrace_filter = Minitest::ExtensibleBacktraceFilter.default_filter
-backtrace_filter.add_filter(%r{gems/sorbet-runtime})
-backtrace_filter.add_filter(%r{gems/railties})
-backtrace_filter.add_filter(%r{tapioca/helpers/test/})
-
-Minitest::Reporters.use!(SpecReporter.new(color: true), ENV, backtrace_filter)
+# Use default minitest reporter (faster than SpecReporter)
 
 require "minitest/mock"
 
